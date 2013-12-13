58b217466662530e505de87296e96a08d05c9977
David Catt <david.werecat@gmail.com>
2013-12-12 21:10:34 -0500

    Merged functions with preprocessor defines

--- FastAri.c
+++ FastAri.c
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+/*#define FA_UNSAFE_DECODE*/
+/*#define FA_USE_EOF*/
 #include <stdlib.h>
 #define ORDER 17
 #define ADAPT 4
@@ -30,6 +32,17 @@ int fa_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, siz
 	ctx = 0;
 	/* Walk through input */
 	for(ipos = 0; ipos < ilen; ++ipos) {
+#ifdef FA_USE_EOF
+		/* Encode not EOF */
+		++low;
+		while ((high ^ low) < 0x1000000) {
+			obuf[opos] = high >> 24;
+			++opos;
+			high = (high << 8) | 255;
+			low <<= 8;
+		}
+#endif
+		/* Encode byte */
 		bv = ibuf[ipos];
 		for(bp = 7; bp >= 0; --bp) {
 			mid = low + (((high - low) >> 16) * mdl[ctx]);
@@ -56,6 +69,9 @@ int fa_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, siz
 	obuf[opos] = (high >> 16) & 0xFF; ++opos;
 	obuf[opos] = (high >> 8) & 0xFF; ++opos;
 	obuf[opos] = high & 0xFF; ++opos;
+#ifdef FA_USE_EOF
+	obuf[opos] = 0; ++opos;
+#endif
 	/* Cleanup */
 	free(mdl);
 	*olen = opos;
@@ -64,6 +80,9 @@ int fa_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, siz
 int fa_decompress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen) {
 	unsigned int low = 0, mid, high = 0xFFFFFFFF, cur = 0, c;
 	size_t opos = 0;
+#ifdef FA_UNSAFE_DECODE
+	size_t len = olen;
+#endif
 	int bp, bv;
 	int ctx;
 	const unsigned char* ibuf_end = ibuf + ilen;
@@ -78,68 +97,20 @@ int fa_decompress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, s
 	cur = (cur << 8) | *ibuf; ++ibuf;
 	cur = (cur << 8) | *ibuf; ++ibuf;
 	/* Walk through input */
+#ifdef FA_UNSAFE_DECODE
+	while(len) {
+#else
 	while(opos < *olen) {
+#endif
+#ifdef FA_USE_EOF
+		if (cur <= low) break;
+		++low;
+#endif
 		bv = 0;
 		for(bp = 0; bp < 8; ++bp) {
 			/* Decode bit */
 			mid = low + (((high - low) >> 16) * mdl[ctx]);
 			if(cur <= mid) {
-				high = mid;
-				mdl[ctx] += (0xFFFF - mdl[ctx]) >> ADAPT;
-				bv |= (1 << bp);
-				ctx = ((ctx << 1) | 1) & mask;
-			} else {
-				low = mid + 1;
-				mdl[ctx] -= mdl[ctx] >> ADAPT;
-				ctx = (ctx << 1) & mask;
-			}
-			while((high ^ low) < 0x1000000) {
-				high = (high << 8) | 0xFF;
-				low <<= 8;
-				c = *ibuf;
-				cur = (cur << 8) | c;
-				if(++ibuf >= ibuf_end) {
-					free(mdl);
-					*olen = opos;
-					return 0;
-				}
-			}
-		}
-		*obuf = bv;
-		++obuf;
-		++opos;
-	}
-	/* Cleanup */
-	free(mdl);
-	if(ibuf_end == ibuf) {
-		*olen = opos;
-		return 0;
-	} else {
-		return 2;
-	}
-}
-int fa_decompress_unsafe(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen) {
-	unsigned int low = 0, mid, high = 0xFFFFFFFF, cur = 0, c;
-	size_t opos = 0, len = *olen;
-	int bp, bv;
-	int ctx;
-	/* Initialize model */
-	unsigned short* mdl = malloc((mask + 1) * sizeof(unsigned short));
-	if(!mdl) return 1;
-	for(ctx = 0; ctx <= mask; ++ctx) mdl[ctx] = 0x8000;
-	ctx = 0;
-	/* Initialize coder */
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	/* Walk through input */
-	while(len) {
-		bv = 0;
-		for(bp = 0; bp < 8; ++bp) {
-			/* Decode bit */
-			mid = low + (((high - low) >> 16) * mdl[ctx]);
-			if (cur <= mid) {
 				high = mid;
 				mdl[ctx] += (0xFFFF - mdl[ctx]) >> ADAPT;
 				bv |= (1 << bp);
@@ -154,175 +125,34 @@ int fa_decompress_unsafe(const unsigned char* ibuf, unsigned char* obuf, size_t 
 				low <<= 8;
 				c = *ibuf; ++ibuf;
 				cur = (cur << 8) | c;
-			}
-		}
-		*obuf = bv;
-		++obuf;
-		--len;
-	}
-	/* Cleanup */
-	free(mdl);
-	return 0;
-}
-int fae_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen) {
-	unsigned int low = 0, mid, high = 0xFFFFFFFF;
-	size_t ipos, opos = 0;
-	int bp, bv;
-	int ctx;
-	/* Initialize model */
-	unsigned short* mdl = malloc((mask + 1) * sizeof(unsigned short));
-	if(!mdl) return 1;
-	for(ctx = 0; ctx <= mask; ++ctx) mdl[ctx] = 0x8000;
-	ctx = 0;
-	/* Walk through input */
-	for(ipos = 0; ipos < ilen; ++ipos) {
-		/* Encode not EOF */
-		++low;
-		while((high ^ low) < 0x1000000) {
-			obuf[opos] = high >> 24;
-			++opos;
-			high = (high << 8) | 255;
-			low <<= 8;
-		}
-		/* Encode byte */
-		bv = ibuf[ipos];
-		for(bp = 7; bp >= 0; --bp) {
-			mid = low + (((high - low) >> 16) * mdl[ctx]);
-			if(bv & 1) {
-				high = mid;
-				mdl[ctx] += (0xFFFF - mdl[ctx]) >> ADAPT;
-				ctx = ((ctx << 1) | 1) & mask;
-			} else {
-				low = mid + 1;
-				mdl[ctx] -= mdl[ctx] >> ADAPT;
-				ctx = (ctx << 1) & mask;
-			}
-			bv >>= 1;
-			while((high ^ low) < 0x1000000) {
-				obuf[opos] = high >> 24;
-				++opos;
-				high = (high << 8) | 0xFF;
-				low <<= 8;
-			}
-		}
-	}
-	/* Flush coder */
-	obuf[opos] = (high >> 24); ++opos;
-	obuf[opos] = (high >> 16) & 0xFF; ++opos;
-	obuf[opos] = (high >> 8) & 0xFF; ++opos;
-	obuf[opos] = high & 0xFF; ++opos;
-	obuf[opos] = 0; ++opos;
-	/* Cleanup */
-	free(mdl);
-	*olen = opos;
-	return 0;
-}
-int fae_decompress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen) {
-	unsigned int low = 0, mid, high = 0xFFFFFFFF, cur = 0, c;
-	size_t opos = 0;
-	int bp, bv;
-	int ctx;
-	const unsigned char* ibuf_end = ibuf + ilen;
-	/* Initialize model */
-	unsigned short* mdl = malloc((mask + 1) * sizeof(unsigned short));
-	if(!mdl) return 1;
-	for(ctx = 0; ctx <= mask; ++ctx) mdl[ctx] = 0x8000;
-	ctx = 0;
-	/* Initialize coder */
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	/* Walk through input */
-	while(opos < *olen) {
-		if(cur <= low) break;
-		++low;
-		bv = 0;
-		for(bp = 0; bp < 8; ++bp) {
-			/* Decode bit */
-			mid = low + (((high - low) >> 16) * mdl[ctx]);
-			if(cur <= mid) {
-				high = mid;
-				mdl[ctx] += (0xFFFF - mdl[ctx]) >> ADAPT;
-				bv |= (1 << bp);
-				ctx = ((ctx << 1) | 1) & mask;
-			} else {
-				low = mid + 1;
-				mdl[ctx] -= mdl[ctx] >> ADAPT;
-				ctx = (ctx << 1) & mask;
-			}
-			while((high ^ low) < 0x1000000) {
-				high = (high << 8) | 0xFF;
-				low <<= 8;
-				c = *ibuf;
-				cur = (cur << 8) | c;
-				if(++ibuf >= ibuf_end) {
+#ifndef FA_UNSAFE_DECODE
+				if(ibuf >= ibuf_end) {
 					free(mdl);
 					*olen = opos;
 					return 0;
 				}
+#endif
 			}
 		}
 		*obuf = bv;
 		++obuf;
+#ifdef FA_UNSAFE_DECODE
+		--len;
+#else
 		++opos;
+#endif
 	}
 	/* Cleanup */
 	free(mdl);
-	if(ibuf_end == ibuf) {
+#ifdef FA_UNSAFE_DECODE
+	return 0;
+#else
+	if (ibuf_end == ibuf) {
 		*olen = opos;
 		return 0;
 	}
 	else {
 		return 2;
 	}
-}
-int fae_decompress_unsafe(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen) {
-	unsigned int low = 0, mid, high = 0xFFFFFFFF, cur = 0, c;
-	size_t opos = 0, len = *olen;
-	int bp, bv;
-	int ctx;
-	/* Initialize model */
-	unsigned short* mdl = malloc((mask + 1) * sizeof(unsigned short));
-	if(!mdl) return 1;
-	for(ctx = 0; ctx <= mask; ++ctx) mdl[ctx] = 0x8000;
-	ctx = 0;
-	/* Initialize coder */
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	cur = (cur << 8) | *ibuf; ++ibuf;
-	/* Walk through input */
-	while(len) {
-		if(cur <= low) break;
-		++low;
-		bv = 0;
-		for(bp = 0; bp < 8; ++bp) {
-			/* Decode bit */
-			mid = low + (((high - low) >> 16) * mdl[ctx]);
-			if (cur <= mid) {
-				high = mid;
-				mdl[ctx] += (0xFFFF - mdl[ctx]) >> ADAPT;
-				bv |= (1 << bp);
-				ctx = ((ctx << 1) | 1) & mask;
-			} else {
-				low = mid + 1;
-				mdl[ctx] -= mdl[ctx] >> ADAPT;
-				ctx = (ctx << 1) & mask;
-			}
-			while ((high ^ low) < 0x1000000) {
-				high = (high << 8) | 0xFF;
-				low <<= 8;
-				c = *ibuf; ++ibuf;
-				cur = (cur << 8) | c;
-			}
-		}
-		*obuf = bv;
-		++obuf;
-		--len;
-	}
-	/* Cleanup */
-	free(mdl);
-	*olen = opos;
-	return 0;
+#endif
 }
--- FastAri.h
+++ FastAri.h
@@ -1,6 +1,2 @@
 int fa_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
 int fa_decompress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
-int fa_decompress_unsafe(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
-int fae_compress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
-int fae_decompress(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
-int fae_decompress_unsafe(const unsigned char* ibuf, unsigned char* obuf, size_t ilen, size_t* olen);
